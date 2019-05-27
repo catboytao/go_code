4033751d97b8cb692f863d1fb58451da68225c2f
catboytao <40749430+catboytao@users.noreply.github.com>
2019-05-27 20:06:12 +0800

    update gocode

--- chapter10/structdemo/main.go
+++ chapter10/structdemo/main.go
@@ -14,9 +14,23 @@ type Cat struct {
 
 func main() {
 	//创建一个Cat对象
+	//方式1
 	var c Cat
 	c.Name = "小白"
 	c.Age = 3
 	c.Color = "白色"
 	fmt.Println("c=",c)
+	//方式2
+	var c2 Cat = Cat{"小花",5,"花色"}
+	fmt.Println("c2=",c2)
+	//方式3
+	var c3 *Cat = new(Cat)
+	(*c3).Name = "小黑"
+	(*c3).Age = 2
+	(*c3).Color = "黑色"
+	fmt.Println("c3=",*c3)
+	//方式4
+	var c4 *Cat = &Cat{"小黄",3,"黄色"}
+	fmt.Println("c4=",*c4)
+
 }
